6fb1a43e3cde36605e5450bc8edecfe375eaf8ee
spoonmilk <haku31229@gmail.com>
2024-12-07 22:32:31 -0500

    fix: adjusted for sparse polynomials

--- poqr/ntru/src/lib.rs
+++ poqr/ntru/src/lib.rs
@@ -1,18 +1,19 @@
 use ark_bn254::Fr;
-use ark_poly::{polynomial::univariate::DensePolynomial, DenseUVPolynomial};
+use ark_poly::{polynomial::univariate::SparsePolynomial};
 use rand::prelude::*;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//NOTE: Functionality implemented tonight (12/7) will likely be sectionalized into an ntru-utils
+//file but i encourage not expanding into complex file structure until needed. For now, just
+//keeping this in the base file :)
 
 /// Generates a random polynomial given a degree bound
-pub fn random_poly(deg_b: usize) -> DensePolynomial<Fr> {
+pub fn random_poly(deg_b: usize) -> SparsePolynomial<Fr> {
     let mut rng = rand::thread_rng();
-    let coeffs: Vec<Fr> = (0..deg_b + 1).map(|_| Fr::from(rng.gen::<i64>())).collect();
-    DensePolynomial::from_coefficients_vec(coeffs)
+    let coeffs: Vec<(usize, Fr)> = (0..deg_b + 1)
+    .map(|i| (i, Fr::from(rng.gen_range(-1..=1))))
+    .collect();
+    SparsePolynomial::from_coefficients_vec(coeffs)
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -26,3 +27,4 @@ mod tests {
         assert_eq!(poly.degree(), 10);
     }
 }
+
