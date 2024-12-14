5d9c1a8c3138d3bd103227e0f282fb6cd91cc67a
spoonmilk <haku31229@gmail.com>
2024-12-14 14:51:27 -0500

    feat: key creation

--- poqr/ntru/src/ntru_key.rs
+++ poqr/ntru/src/ntru_key.rs
@@ -1,6 +1,6 @@
 use crate::convolution_polynomial::{ternary_polynomial, ConvPoly};
-use crate::params::*;
 use crate::ntru_key::*;
+use crate::params::*;
 
 /// An NTRU key pair
 pub struct NtruKeyPair {
@@ -10,14 +10,27 @@ pub struct NtruKeyPair {
     pub private_key: NtruPrivateKey,
 }
 impl NtruKeyPair {
-    pub fn gen_keypair() -> NtruKeyPair {}
+    pub fn new() -> NtruKeyPair {
+        let k_priv = NtruPrivateKey::new();
+        let k_pub = NtruPublickKey::new(k_priv);
+        NtruKeyPair {
+            public_key: k_pub,
+            private_key: k_priv,
+        }
+    }
 }
 
 /// A public key used in the NTRU encryption scheme
 pub struct NtruPublicKey {
     h: ConvPoly,
 }
-impl NtruPublicKey {}
+impl NtruPublicKey {
+    fn new(k_priv: NtruPrivateKey) -> NtruPublicKey {
+        let f_inv = k_priv.f.inverse(Q).unwrap();
+        let h = f_inv.mul(&k_priv.g);
+        NtruPublicKey { h }
+    }
+}
 
 /// A private key used in the NTRU encryption scheme
 pub struct NtruPrivateKey {
@@ -27,7 +40,17 @@ pub struct NtruPrivateKey {
 impl NtruPrivateKey {
     fn new() -> NtruPrivateKey {
         let f: ConvPoly = {
-            let f: ConvPoly = ternary_polynomial()
-        } 
+            let mut f: ConvPoly = ternary_polynomial(N, D + 1, D);
+            let mut valid_poly: bool = false;
+            while !valid_poly {
+                if f.inverse(Q).is_ok() && f.inverse(P).is_ok() {
+                    valid_poly = true;
+                }
+                f = ternary_polynomial(N, D + 1, D)
+            }
+            f
+        };
+        let g: ConvPoly = ternary_polynomial(N, D, D);
+        NtruPrivateKey { f, g }
     }
 }
