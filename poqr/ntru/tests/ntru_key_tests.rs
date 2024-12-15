f1e30fe844cbb0692951362312895b1546186534
Tanish Makadia <tanishmakadia@gmail.com>
2024-12-15 17:22:30 -0500

    test: Add a test for encryption-decryption that uses random messages

--- poqr/ntru/tests/ntru_key_tests.rs
+++ poqr/ntru/tests/ntru_key_tests.rs
@@ -1,15 +1,32 @@
 #[cfg(test)]
 mod ntru_key_tests {
     use ntru::ntru_key::NtruKeyPair;
+    use rand::Rng;
 
     #[test]
     fn test_ntru_encrypt_decrypt() {
+        // "Hello World" message
         let keypair = NtruKeyPair::new();
-        let msg = "hello world".as_bytes().to_vec();
+        let msg = "Hello World".as_bytes().to_vec();
         println!("Message: {:?}", msg);
         let enc_msg = keypair.encrypt(msg.clone());
         let dec_msg = keypair.decrypt(enc_msg);
         println!("Decrypted message: {:?}", dec_msg);
         assert_eq!(msg, dec_msg);
+
+        // Test empty message
+
+        // Test random messages
+        let num_tests = 100;
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..num_tests {
+            let msg_len = rng.gen_range(0..=32);
+            let keypair = NtruKeyPair::new();
+            let msg: Vec<u8> = (0..msg_len).map(|_| rng.gen_range(1..=127)).collect();
+            let enc_msg = keypair.encrypt(msg.clone());
+            let dec_msg = keypair.decrypt(enc_msg);
+            assert_eq!(msg, dec_msg);
+        }
     }
 }
